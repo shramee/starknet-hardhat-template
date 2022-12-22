4c6f149d6acc8edcb4e7ac1810593c513da823b6
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-12-22 14:04:26 +0700

    Adding starknet-hardhat-plugin

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@shardlabs/starknet-hardhat-plugin";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
